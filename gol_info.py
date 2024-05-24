f87ed17149ea8a326b6b124afc0db5b085eb0616
dinosaurioinvisible <ferodriguezv@gmail.com>
2024-05-24 16:17:41 +0100

    all txs data into gol_delta_map

--- gol_info.py
+++ gol_info.py
@@ -1,21 +1,50 @@
 from gol_info_utils import *
 # from gol_fxs import *
+# from scipy.special import kl_div
 # import pandas as pd
 
+# compare exs vs Exs
 
-# pb0_sx,pb0_dx = mk_gol_pattern('pb0',domain=True)
-# pb0_xs,pb0_ys,pb0_syms,pb0_cases,pb0_ids = load_data('xgol_pb0_cap=7.xgol')
-# pb0_uf = mk_uniform_ids(pb0.sx,'pt-block',pb0_syms,pb0_cases)
 
-# sx & sx domain
-# dxs and dxys (all)
-# dxs and dxys (caps)
-# sxys syms & sym cases 
-# 
 
-px = blinker
-fname = f'gol_domains_cap=10_{px.label}'
-dxs = load_data(filename=fname)
-dys = multi_gol_tx(dxs)
+
+
+# kl divergence
+def compare_kldiv(px,py_label):
+    txs = px.txs[py_label]
+    dxs_fname = f'gol_domains_cap=10_{px.label}'
+    dxs = load_data(filename=dxs_fname)
+
+    env_set = px.env_sets[py_label].flatten()
+    env_ids = px.env.flatten().nonzero()[0]
+    env_ct = env_set[env_ids]
+    kl_infos = []
+    info_acum = 0
+    print(f'\n{px.label} -> {py_label}')
+    for ei,tx_id in enumerate(txs):
+        print(f'{ei} - id: {tx_id}')
+
+        env = dxs[tx_id].flatten()[env_ids]
+        print(f'env: {env}')
+        print(f'env cat: {env_ct}\n')
+
+        kld_info = kl_div(env,env_ct)
+        print(f'scipy: {kld_info}')
+        print('KLD(P||Q): P=env, Q=cog. Info lost when approx using Q instead of P')
+        kld_info_pq = np.sum(np.where(env!=0, env*np.log(env/env_ct), 0))
+        print(f'KLD Q=cog info: {kld_info_pq}')
+        info_acum += kld_info_pq
+        kl_infos.append(kld_info_pq)
+        print('KLD(Q|P): ?')
+        kld_info_qp = np.sum(np.where(env_ct!=0, env_ct*np.log(env_ct/env), 0))
+        print(f'KLD Q=env info: {kld_info_qp}')
+
+        print(f'acum. info: {info_acum}\n')
+        import pdb; pdb.set_trace()
+
+    print(f'\nacum. info: {info_acum}\n')
+    return np.array(kl_infos)
+
+
 
 
--- gol_info_utils.py
+++ gol_info_utils.py
@@ -1,40 +1,101 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 from gol_auxs import mk_gol_pattern, mk_sx_variants
-from gol_fxs import load_data
+from tqdm import tqdm
 
+# sx, dx, etc. astype int?
 class GolPattern:
-    def __init__(self, name, domains=False):
+    def __init__(self, name, txs=False):
         self.label = name
         self.sx, self.dx = mk_gol_pattern(self.label, domain=True)
         self.mb = mk_moore_nb(self.dx)                 # membrane
         self.env = mk_moore_nb(self.dx + self.mb)      # environment
         self.mk_rec_vxs()
-        if domains:
-            self.load_domains()
+        # self.txs = {}
+        self.all_txs, self.ntxs = 0, 0
+        self.env_sets = {}
+        if txs:
+            self.get_txs_data()
 
-    def load_domains(self):
-        self.dxs, self.dys, self.syms, self.cases, ids = load_data(filename=self.label)
-        
     def mk_rec_vxs(self):
         vxs = mk_sx_variants(self.sx, mk_non_env=True)
-        self.vxs = np.rec.array(None, dtype=[('sxs',np.ndarray),
+        self.vxs = np.rec.array(None, dtype=[('sx',np.ndarray),
                                              ('e0',np.bool_),
                                              ('ecells',np.uint8),
                                              ('id',np.uint64)],
                                              shape = len(vxs))
         for ei,vxi in enumerate(vxs):
-            self.vxs[ei].sxs = vxi
+            self.vxs[ei].sx = vxi
             ecells = vxi.nonzero()[0].shape[0] - self.sx.nonzero()[0].shape[0]
             self.vxs[ei].ecells = ecells
             self.vxs[ei].e0 = True if ecells == 0 else False
-            self.vxs[ei].id = array2int(vxi)
+            env_mask = np.ones((vxi.shape))
+            env_mask[1:-1,1:-1] = 0     # so sx & sx+env have the same id
+            self.vxs[ei].id = array2int(vxi*env_mask)
         
-        self.nsxs = len(vxs)
+        self.nvxs = len(vxs)
+        self.sxs0 = self.vxs.sx[self.vxs.ecells==False]
 
-    def sxs0(self):
-        print(f'\nenv0 cases: {sum(self.vxs.ecells==False)}\n')
-        return self.vxs.sxs[self.vxs.ecells==False]
+    def get_txs_data(self):
+        fname = f'gol_px_txs_ct_{self.label}'
+        self.txs = load_data(filename=fname)
+        # clean txs info: CT in dy?
+        self.txs_info()
+        self.mk_env_sets()
+        
+    def txs_info(self):
+        print('{}'.format(f'\n{self.label} ->' if len(self.txs.keys())>0 else '\nno txs info\n'))
+        self.ntxs = 0
+        for key in self.txs.keys():
+            self.ntxs += self.txs[key].shape[0]
+            print(f'{key}: {self.txs[key].shape[0]}')
+        print(f'{self.ntxs} txs known')
+        if self.all_txs:
+            print(f'{self.all_txs - self.ntxs}/{self.all_txs} txs unknown')
+
+    def mk_env_sets(self):
+        dxs_fname = f'gol_domains_cap=10_{self.label}'
+        # dys_fname = f'gol_tx_domains_cap=10_{px.label}'
+        dxs = load_data(filename=dxs_fname)
+        # dys = load_data(filename=dys_fname)
+        self.all_txs = dxs.shape[0]
+        for key in self.txs.keys():
+            dxs_sums = np.zeros((self.dx.shape)).astype(int)
+            for dx_id in self.txs[key]:
+                dxs_sums += dxs[dx_id]
+            dxs_sums *= np.abs(self.dx - 1).astype(int)
+            self.env_sets[key] = dxs_sums/dxs_sums.sum()
+
+    def plot_domains(self):
+        mk_plots = True
+        while mk_plots:
+            print(f'\n{self.label} ->')
+            for ei,key in enumerate(self.env_sets.keys()):
+                print(f'[{ei}] -> {key}, ncases = {self.txs[key].shape[0]}')
+            kx = input('\n? _ ')
+            if kx == 'q' or kx == 'quit':
+                mk_plots = False
+            else:
+                try:
+                    # info
+                    # plots
+                    kx_label = list(self.env_sets.keys())[int(kx)]
+                    domain = self.env_sets[kx_label]
+                    # cmap = mpl.cm.get_cmap("jet").copy()
+                    non_dx = self.dx + self.mb + self.env - 1
+                    domain += non_dx
+                    domain += self.mb * -1
+                    domain += self.dx * 2
+                    ex_dx = self.env * domain
+                    imx = plt.imshow(domain, vmin=0, vmax=np.max(ex_dx), cmap='magma', aspect='auto')
+                    imx.cmap.set_over('black')
+                    imx.cmap.set_under('white')
+                    plt.colorbar()
+                    plt.title(f'{self.label} -> {kx_label} environmental category')
+                    plt.show()
+                except:
+                    import pdb; pdb.set_trace()
 
 # array to int
 def array2int(arr):
@@ -96,7 +157,7 @@ def mk_tensor(mx,tx):
         return mx
     return mx.reshape(mx.shape[0],tx.shape[0],tx.shape[1])
 
-def mk_px_domains(px,cap=10,tensor=True,save=False):
+def mk_px_domains(px,cap=10,save=False):
     env_bin_dxs = mk_binary_domains(px.env.sum())
     px_env_dxs = np.zeros((env_bin_dxs.shape[0],px.dx.flatten().shape[0])).astype(int)
     env_ids = px.env.flatten().nonzero()[0]
@@ -106,13 +167,16 @@ def mk_px_domains(px,cap=10,tensor=True,save=False):
         px_env_dxs = px_env_dxs[px_env_dxs.sum(axis=1)<=cap]       
     sx_ids = px.dx.flatten().nonzero()[0]
     px_env_dxs[:,sx_ids] = 1
-    if tensor:
-        px_env_dxs = mk_tensor(px_env_dxs,px.dx)
+    px_env_dxs = mk_tensor(px_env_dxs,px.dx)
     if save:
         fname = f'gol_domains_cap={cap}_{px.label}'
         save_as(px_env_dxs,name=fname)
         return
     return px_env_dxs
+
+# def delta_xy(dx,dy):
+#     dxy = np.abs(dy - dx)
+#     return dxy
 
 # gol transition step (dy: dx expanded for new ON cells)
 def gol_tx(dx):
@@ -121,15 +185,147 @@ def gol_tx(dx):
     for ei in range(dx.shape[0]):
         for ej in range(dx.shape[1]):
             nb = dx[max(0,ei-1):ei+2,max(0,ej-1):ej+2].sum() - dx[ei,ej]
+            # cell = 0 or 1 and nb=3, or cell=1 and nb=2
             dy[ei,ej] = 1 if nb==3 or dx[ei,ej]*nb==2 else 0
     return dy
 def multi_gol_tx(dxs):
-    from tqdm import tqdm
     dys = np.zeros((dxs.shape[0],dxs.shape[1]+2,dxs.shape[2]+2)).astype(int)
     for di in tqdm(range(dxs.shape[0])):
         dys[di] = gol_tx(dxs[di])
     return dys
 
+# sliding window matching sx (2d) in all dxs (3d)
+# optional pad (for pxs ct would be false in padded borders)
+def is_sx_in_dxs(sx,dxs,pad=False):
+    if pad:
+        dxs = np.pad(dxs,((0,0),(1,1),(1,1)))
+    sxsum = sx.sum()
+    # sx_nz = np.ones((sx.shape)).astype(int) - sx
+    ids = np.zeros(dxs.shape[0]).astype(int)
+    for wi in range(dxs.shape[1]-sx.shape[0]+1):
+        for wj in range(dxs.shape[2]-sx.shape[1]+1):
+            wids = np.zeros(dxs.shape[0])
+            wx = dxs[:,wi:wi+sx.shape[0],wj:wj+sx.shape[1]]
+            # sx is there, domain unknown; only sx in domain
+            wids[np.sum(wx*sx,axis=(1,2))==sxsum] += 0.5
+            wids[wx.sum(axis=(1,2))==sxsum] += 0.5
+            # remove if env is non zero 
+            # wids[np.sum(wx*sx_nz,axis=(1,2))>0] = 0
+            ids += wids.astype(int)            
+    return ids
+
+# dx/dy ids for px -> py transition
+def mk_pxpy_txs(px,pxs, dx_contraint=True):
+    px_fname = f'gol_tx_domains_cap=10_{px.label}'
+    px_dys = load_data(filename=px_fname)
+    # if dx_contraint:
+    di,dj = px_dys.shape[1:]
+    vi,vj = 0,0
+        # patterns outside dx space are made from env
+        # px_dys = px_dys[:,1:-1,1:-1]
+    px_txs = {}
+    for py in pxs:
+        print(f'\n{px.label} -> {py.label:}')
+        ids = np.zeros(px_dys.shape[0]).astype(int)
+        for i in tqdm(range(py.vxs.sx.shape[0])):
+            sy = py.vxs[i].sx
+            # patterns outside dx space are made from env
+            if dx_contraint:
+                vi,vj = (np.array(px_dys.shape[1:] - np.array(sy.shape) -2)/2).astype(int)
+            ids += is_sx_in_dxs(sy,px_dys[:,vi:di-vi,vj:dj-vj])
+            # ids += is_sx_in_dxs(sy,px_dys)
+        # is there more than 1 sx in dx?
+        if ids[ids>1].shape[0] > 0:
+            print(f'\nmore than one sx?:\n{ids[ids>1].shape[0]} cases\n{ids[ids>1]}')
+            examples = np.where(ids>1)[0][:5]
+            for exi in examples:
+                print(f'\n{exi}')
+                print(px_dys[exi])
+            # import pdb; pdb.set_trace()
+        px_txs[py.label] = np.where(ids>0)[0]
+    # disintegrations
+    px_txs['end'] = np.where(np.sum(px_dys[:,2:-2,2:-2]*px.sx,axis=(1,2))<1)[0]
+    # info
+    all_txs = px_dys.shape[0]
+    ntxs = 0
+    print()
+    for key in px_txs.keys():
+        ntxs += px_txs[key].shape[0]
+        print(f'{key}: {px_txs[key].shape[0]}')
+    print(f'{ntxs} txs known, {all_txs - ntxs}/{all_txs} unknown')
+    # return np.where(ids>0)[0]
+    # save_as(px_txs,name=f'gol_px_txs_{px.label}')
+    fname = 'gol_px_txs_{}{}'.format('ct_' if dx_contraint else '', px.label)
+    save_as(px_txs,name=fname)
+
+def mk_omap(pxs):
+    omap = {}
+    labels = []
+    for px in pxs:
+        labels.append(px.label)
+        omap[px.label] = {}
+        omap[px.label]['fwd'] = {}
+        omap[px.label]['back'] = {}
+        omap[px.label]['nfwd'] = {}
+        omap[px.label]['nback'] = {}
+        omap[px.label]['edx'] = {}
+    print()
+    for px in pxs:
+        for li in tqdm(range(len(labels))):
+            py_label = labels[li]
+            omap[px.label]['fwd'][py_label] = px.txs[py_label]
+            omap[py_label]['back'][px.label] = px.txs[py_label]
+            omap[px.label]['nfwd'][py_label] = px.txs[py_label].shape[0]
+            omap[py_label]['nback'][px.label] = px.txs[py_label].shape[0]
+            omap[px.label]['edx'][py_label] = px.env_sets[py_label]
+    return omap
+
+# sx -> sy : tx ids : env set
+# sx -> sy : n txs : env set weight/cardinality
+# sx <- sy : tx ids : backwards env set?
+# sx <- sy : n txs : backwards weight/cardinality
+# sx -> sy : dom x
+# sx -> sy : dom y
+# sx -> sy : delta sx,sy
+# sx -> sy : eks : environmental distinctions
+# sx -> sy : dom y sums
+def mk_delta_map(pxs,omap):
+    # make dicts
+    dmap = {}
+    dmap['labels'] = list(omap.keys())
+    for px in pxs:
+        dmap[px.label] = {}
+        for ku in ['fwd', 'nfwd', 'back', 'nback', 'env_dks', 'dxs', 'dys', 'sxy', 'dysums']:
+            dmap[px.label][ku] = {}
+    # direct copy from omap
+    for px in pxs:
+        for py_label in dmap['labels']:
+            # fwd tx ids, n, bwd tx ids, n, env distinctions
+            for txu in ['fwd', 'nfwd', 'back', 'nback']:
+                dmap[px.label][txu][py_label] = omap[px.label][txu][py_label]
+                dmap[px.label]['env_dks'][py_label] = omap[px.label]['edx'][py_label]
+    # load domains data
+    for px in pxs:
+        px_dxs = load_data(filename=f'gol_domains_cap=10_{px.label}')
+        px_dys = load_data(filename=f'gol_tx_domains_cap=10_{px.label}')
+        px_dys_ct = px_dys[:,1:-1,1:-1]
+        pxdx = px.dx.astype(int)
+        for py_label in dmap['labels']:
+            # sx domains, sy domains, sxy transition, sy domain sums 
+            dxs_tensor = np.zeros((omap[px.label]['nfwd'][py_label],pxdx.shape[0],pxdx.shape[1])).astype(int)
+            dys_tensor = np.zeros((omap[px.label]['nfwd'][py_label],pxdx.shape[0]+2,pxdx.shape[1]+2)).astype(int)
+            sxy_tensor = np.zeros((omap[px.label]['nfwd'][py_label],pxdx.shape[0],pxdx.shape[1])).astype(int)
+            for ei,id in enumerate(omap[px.label]['fwd'][py_label]):
+                dxs_tensor[ei] = px_dxs[id]
+                dys_tensor[ei] = px_dys[id]
+                sxy_tensor[ei] = pxdx + px_dys_ct[id]
+            dmap[px.label]['dxs'][py_label] = dxs_tensor
+            dmap[px.label]['dys'][py_label] = dys_tensor
+            dmap[px.label]['sxy'][py_label] = sxy_tensor
+            dmap[px.label]['dysums'] = dys_tensor.sum(axis=0)[1:-1,1:-1]
+    fname = f'gol_delta_map_cap=10'
+    save_as(dmap, name=fname)
+    # return dmap
 
 '''save, load, etc'''
 def save_as(file,name,fdir='gol_exps_data',ext='gol_info'):
@@ -143,9 +339,10 @@ def save_as(file,name,fdir='gol_exps_data',ext='gol_info'):
     if not os.path.isdir(fdir):
         os.mkdir(fdir)
     fname = os.path.join(fdir,fname)
-    if os.path.isfile(fname):
+    while os.path.isfile(fname):
         print(f'\nfile already exists at {fname}\n')
-        return
+        fname += '_(new)'
+        # return
     with open(fname,'wb') as f:
         pickle.dump(file,f)
     print('\nsaved as: {}\n'.format(fname))
@@ -174,11 +371,15 @@ def load_data(filename='',ext='gol_info',dirname='gol_exps_data'):
         if x == 'q' or x == 'quit':
             return
         try:
-            with open(fnames[int(x)-1],'rb') as fname:
+            fpath = os.path.join(dirpath,fnames[int(x)-1])
+            with open(fpath,'rb') as fname:
                 fdata = pickle.load(fname)
                 return fdata
         except:
+            fpath = os.path.join(dirpath,fnames[int(x)-1])
+            import pdb; pdb.set_trace()
             print('\ninvalid input? (q/quit to quit)\n')
+
 
 blinker = GolPattern('blinker')
 pb0 = GolPattern('pb0')
@@ -197,13 +398,33 @@ worm = GolPattern('worm')
 
 pxs = [blinker,pb0,block,gla,glb,ttz,ttt,ttl,zz,bar,baby,flag,kyte,worm]
 
-# make domains
+# 1) make domains (dxs)
 # for px in pxs:
 #     mk_px_domains(px,save=True)
-for px in pxs:
-    fname = f'gol_domains_cap=10_{px.label}'
-    dxs = load_data(filename=fname)
-    dys = multi_gol_tx(dxs)
-    fname = f'gol_tx_domains_cap=10_{px.label}'
-    save_as(dys,name=fname)
 
+# 2) make co-domains (dys) (GoL txs)
+# for px in pxs:
+#     fname = f'gol_domains_cap=10_{px.label}'
+#     dxs = load_data(filename=fname)
+#     dys = multi_gol_tx(dxs)
+#     fname = f'gol_tx_domains_cap=10_{px.label}'
+#     save_as(dys,name=fname)
+
+# 3) get txs (dxs -> dys) data (labels, ids & n txs)
+# from copy import deepcopy
+# pxs_cp = [deepcopy(px) for px in pxs]
+# for px in pxs:
+#     mk_pxpy_txs(px,pxs_cp)
+
+# 4) make txs map, dict for graph-like data
+# (change txs=True in GolPattern class)
+# omap = mk_omap(pxs)
+# fname = f'gol_omap_cap=10'
+# save_as(omap, name=fname)
+
+# dxs = load_data(filename=f'gol_tx_domains_cap=10_{px.label}')
+# omap = load_data(filename='gol_omap_cap=10')
+
+# 5) make delta(sx,sy) for info comparisons & put all together
+# mk_delta_map(pxs,omap)
+dmap = load_data(filename='gol_delta_map_cap=10')
